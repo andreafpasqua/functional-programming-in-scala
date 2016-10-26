43735e50750f39a7ed277d6eeef951a722c99fdd
Andrea Pasqua <generic072@att.net>
2016-10-26 09:32:07 -0700

    parser work in progress

--- src/main/scala/andrea/scala/functional/programming/parsing/Parser.scala
+++ src/main/scala/andrea/scala/functional/programming/parsing/Parser.scala
@@ -3,13 +3,15 @@ package andrea.scala.functional.programming.parsing
 import andrea.scala.functional.programming.testing.{Prop, Sampler}
 import andrea.scala.functional.programming.either.{Either, Left, Right}
 
+import scala.util.matching.Regex
+
 /**
   * Created by andreapasqua on 10/24/2016.
   */
 
 case class Parser[E, +T](run: String => Either[E, T]) {
 
-  def or[TT >: T](other: Parser[E, TT]): Parser[E, TT] = Parser(
+  def or[TT >: T](other: => Parser[E, TT]): Parser[E, TT] = Parser(
     string => run(string).left.orElse(other.run(string))
   )
 
@@ -30,19 +32,36 @@ case class Parser[E, +T](run: String => Either[E, T]) {
     */
   def many1: Parser[E, List[T]] = map2(many) { case (t, list) => t :: list}
 
-  def listOfN(n: Int): Parser[E, List[T]] = many.map(list => if (list.length == n))
+  /**
+    * A parser that looks at the beginning of the input string
+    * for n subsequent instances of the substring this is sensitive to.
+    * Exercise 9.4
+    */
+  def listOfN(n: Int): Parser[E, List[T]] =
+    if (n > 0) map2(listOfN(n - 1))(_ :: _) else Parser.succeed(Nil)
+
 
   /**
     * Parses the string with this and then maps the result (if any) with f
+    * Exercise 9.8
     */
-  def map[S](f: T => S): Parser[E, S] = Parser(run(_).right.map(f))
+  def map[S](f: T => S): Parser[E, S] = flatMap(t => Parser.succeed(f(t)))
 
   /**
     * Parses the string with this and then parses what is left with other
     * and then combines the results using f
+    * Exercise 9.7
     */
-  def map2[S, U](other: Parser[E, S])(f: (T, S) => U): Parser[E, U] = ???
+  def map2[S, U](other: => Parser[E, S])(f: (T, S) => U): Parser[E, U] =
+    for {
+      t <- this
+      s <- other
+    } yield f(t, s)
 
+  /**
+    * Parses a string and then uses a function f to obtain from the result
+    * a new parser that is used to parse the remainder of the string
+    */
   def flatMap[S](f: T => Parser[E, S]): Parser[E, S] = ???
 
   /**
@@ -56,13 +75,30 @@ case class Parser[E, +T](run: String => Either[E, T]) {
     * both outputs as a tuple or a failure in all other cases.
     * Exercise 9.1
     */
-  def product[S](other: Parser[E, S]): Parser[E, (T, S)] = map2(other)((_, _))
+  def product[S](other: => Parser[E, S]): Parser[E, (T, S)] = map2(other)((_, _))
 
   /**
     * Same as product
     */
-  def **[S](other: Parser[E, S]): Parser[E, (T, S)] = product(other)
+  def **[S](other: => Parser[E, S]): Parser[E, (T, S)] = product(other)
 
+  /**
+    * It uses this to delete the corresponding portion of the input and
+    * then parse what is left with other
+    */
+  def >[S](other: Parser[E, S]): Parser[E, S] = **(other).map(_._2)
+
+  /**
+    * It uses this to parse and other to delete its corresponding portion of the input
+    * from what is left after this
+    */
+  def <[S](other: Parser[E, S]): Parser[E, T] = **(other).map(_._1)
+
+  /**
+    * Removes leading and trailing spaces from the string input before parsing it
+    * with this
+    */
+  def trimmed: Parser[E, T] = Parser.char[E](' ').many > this < Parser.char[E](' ')
 }
 
 object Parser {
@@ -105,6 +141,14 @@ object Parser {
   def string[E](s: String): Parser[E, String] = ???
 
   /**
+    * A parser that returns a string fed to it when it matches
+    * the regular expression r
+    *
+    * @return
+    */
+  def regex[E](r: Regex): Parser[E, String] = ???
+
+  /**
     * A parser that recognizes character c and counts how many times in a row it
     * is present at the beginning of the input string
     */
@@ -113,20 +157,34 @@ object Parser {
   /**
     * Same as countChar, but it fails if there is not at least one instance of c
     */
-  def countChar1[String](c: Char): Parser[String, Int] = char(c).many1.slice.map(_.length)
+  def countChar1[E](c: Char): Parser[E, Int] = char(c).many1.slice.map(_.length)
 
   /**
     * Counts how many times c1 occurs at the beginning of the string, and how many times
     * c2 occurs at the beginning of what is left. But it fails if the second count is zero.
     * In case of success it presents the two results as a tuple.
     */
-  def countTwoChars(c1: Char, c2: Char) = countChar(c1) ** countChar1(c2)
+  def countTwoChars[E](c1: Char, c2: Char) = countChar[E](c1) ** countChar1[E](c2)
+
+  /**
+    * Parses a single digit if it is followed by as many instances of the
+    * character c
+    */
+  def numFollowedByAsMany[E](c: Char): Parser[E, Int] =
+    for {
+      digit <- regex[E]("[0-9]".r)
+      _ <- char(c).listOfN(digit.toInt)
+    } yield digit.toInt
 
   implicit def stringToParser(s: String): Parser[ParseError, String] = string[ParseError](s)
+
+  implicit def regexToParser(r: Regex): Parser[ParseError, String] = regex[ParseError](r)
 
 }
 
 object ParserLaws {
+
+  implicit val alphabet = "abcd".toVector
 
   /**
     * A utility to test that two parsers p1, p2 are equivalent, in the same that
@@ -187,5 +245,48 @@ object ParserLaws {
           mapBefore.run(s) == mapAfter.run(s)
       }
     }
+
+}
+
+
+object JSONParser {
+
+  import Parser._
+
+  sealed trait JSON
+  case object JNull extends JSON
+  case class JNumber(get: Double) extends JSON
+  case class JString(get: String) extends JSON
+  case class JBool(get: Boolean) extends JSON
+  case class JArray(get: IndexedSeq[JSON]) extends JSON
+  case class JObject(get: Map[String, JSON]) extends JSON
+
+  def jParser[P]: Parser[P, JSON] =
+    jNull[P] | jNumber[P] | jString[P] | jBool[P] | jArray[P] | jObject[P]
+
+  def jNull[P]: Parser[P, JSON] = string("null").map(_ => JNull)
+
+  def jNumber[P]: Parser[P, JSON] = regex[P]("\d".r).map(d => JNumber(d.toDouble))
+
+  def jString[P]: Parser[P, JSON] = (char[P]('"') ** regex[P]("""\w"&&[^"]""".r) ** char[P]('"')).map {
+    case ((_, w), _) => JString(w)
+  }
+
+  def jBool[P]: Parser[P, JSON] = regex[P]("[true, false]".r)
+    .map(s => if (s == "true") JBool(true) else JBool(false))
+
+  def splitIn[P, T](p: Parser[P, T], c: Char = ','): Parser[P, List[T]] =
+    (p ** ( char[P](c) > p).many).map {case (t, l) => t :: l}
+
+  def jArray[P]: Parser[P, JSON] = {
+    val jsonList = char[P]('[') > splitIn(jParser[P].trimmed) < char[P](']')
+    jsonList.map(list => JArray(list.toVector))
+  }
+
+  def jObject[P]: Parser[P, JSON] = {
+    val kVpair = jString[P].trimmed.slice ** (char[P](':') > jParser[P].trimmed)
+    val jsonList = char[P]('{') > splitIn(kVpair) < char[P]('}')
+    jsonList.map(list => JObject(list.toMap))
+  }
 
 }
